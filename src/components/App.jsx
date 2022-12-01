5751a32ecc5da90cf9e40626b6c510ae5df9ac57
Aadetya Chauhan <aadetya123@gmail.com>
2022-12-01 22:58:40 +0530

    Updating files

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <CssBaseline />
+    <main>
+      <Routes>
+        <Route exact path="/movie/:id" element={<h1>Movie Information</h1>} />
+        <Route exact path="/actors/:id" element={<h1>Actors</h1>} />
+        <Route exact path="/" element={<h1>Movies</h1>} />
+        <Route exact path="/profile/:id" element={<h1>Profile</h1>} />
+      </Routes>
+    </main>
+  </div>
+);
+
+export default App;
